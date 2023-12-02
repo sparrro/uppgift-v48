cf26127b3c0f439499777cf4eb8bc2b830dc6e16
sparrro <116090214+sparrro@users.noreply.github.com>
2023-12-02 18:36:06 +0100

    planeternas positioner

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const sun = document.getElementById('sun')
+const mercury = document.getElementById('mercury')
+const venus = document.getElementById('venus')
+const earth = document.getElementById('earth')
+const mars = document.getElementById('mars')
+const jupiter = document.getElementById('jupiter')
+const saturn = document.getElementById('saturn')
+const uranus = document.getElementById('uranus')
+const neptune = document.getElementById('neptune')
+
+let canclick = true
+let clickedPlanet
